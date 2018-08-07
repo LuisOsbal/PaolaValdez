7b75bdbc8e96066af3fb5a4911afe7fb7c7f46c8
Luis Osnet <luisosnet@gmail.com>
2018-08-07 00:03:10 -0500

    add sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get 'welcome/index'
     root 'welcome#index'
+    
+    resources :abouts, path: I18n.locale == :es ? 'sobre-mi' : :about
+    resources :projects, path: I18n.locale == :es ? 'proyectos' : :projects
+    resources :articles, path: I18n.locale == :es ? 'blog' : :blog
+    resources :contacts, path: I18n.locale == :es ? 'contacto' : :contact
   end
 end
