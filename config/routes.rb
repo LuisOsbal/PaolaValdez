193670c83a41f8146fed466228169c18c6197d3f
Luis Osnet <luisosnet@gmail.com>
2018-08-07 00:08:48 -0500

    add haml index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get 'welcome/index'
     root 'welcome#index'
-    
+
     resources :abouts, path: I18n.locale == :es ? 'sobre-mi' : :about
     resources :projects, path: I18n.locale == :es ? 'proyectos' : :projects
     resources :articles, path: I18n.locale == :es ? 'blog' : :blog
