6fbd10322930b5e173bbe9121750c96c320c1af5
Luis Osnet <luisosnet@gmail.com>
2018-08-16 17:11:26 -0500

    add fake logo

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 # USe bulma.io as framework
 gem 'bulma-rails', '~> 0.7.1'
+gem "animate-rails"
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
---
+++ db/migrate/20180807052604_create_companies.rb
@@ -0,0 +1,19 @@
+class CreateCompanies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :companies do |t|
+      t.text :brand_logo_url
+      t.text :brand_url
+      t.string :brand_contact
+      t.string :title
+      t.string :position
+      t.string :description
+      t.string :content
+      t.jsonb :activities
+      t.datetime :start_date
+      t.datetime :end_date
+      t.boolean :status, default: false
+
+      t.timestamps
+    end
+  end
+end
