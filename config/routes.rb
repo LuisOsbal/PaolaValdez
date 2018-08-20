f844208335b695349b60fc09c5b560373b56c944
Luis Osnet <luisosnet@gmail.com>
2018-08-20 12:11:37 -0500

    change router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get 'welcome/index'
     root 'welcome#index'
-
+    devise_for :users
     resources :abouts, path: I18n.locale == :es ? 'sobre-mi' : :about
     resources :projects, path: I18n.locale == :es ? 'proyectos' : :projects
     resources :articles, path: I18n.locale == :es ? 'blog' : :blog
